8c19331c8219f57f8c4cc6a361449288c67d2e7a
predication <ricketts30@yahoo.com[user]>
2013-03-20 11:10:47 +0000

    added an example of Lazy<T>

---
+++ DEV/ExpConApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Predication.Experiment.Library;
+
+namespace ExpConApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(".NET 4.5 Experiments");
+            
+            LazyExample example = new LazyExample();
+            example.Demonstrate();
+
+            Console.WriteLine("\n\nPress ENTER to Quit.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DEV/Predication.Experiment.Library/Lazy.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Predication.Experiment.Library
+{
+    public class LazyExample
+    {
+        public void Demonstrate()
+        {
+            DateTime functionStart = DateTime.Now;
+            Lazy<ExpensiveTimestampedClass> lazyObject = new Lazy<ExpensiveTimestampedClass>();
+            Console.WriteLine("{0} : function started", functionStart.TimeOfDay);
+            Console.WriteLine("{0} : Lazy<T> created", DateTime.Now);
+            Console.WriteLine("{0} : lazyObject.IsValueCreated = {1}", DateTime.Now, lazyObject.IsValueCreated);
+            Console.WriteLine("Press a key to continue");
+            Console.ReadKey();
+            Console.WriteLine("{0} : lazy.Value.Timestamp = {1}", DateTime.Now, lazyObject.Value.Timestamp);
+            Console.WriteLine("{0} : lazyObject.IsValueCreated = {1}", DateTime.Now, lazyObject.IsValueCreated);
+            Console.WriteLine("Press a key to continue");
+            Console.ReadKey();
+            Console.WriteLine("{0} : Static Timestamp = {1}", DateTime.Now, ExpensiveTimestampedClass.StaticTimestamp);
+        }
+    }
+
+    public class ExpensiveTimestampedClass
+    {
+        public static DateTime StaticTimestamp { get; private set; }
+
+        static ExpensiveTimestampedClass()
+        {
+            StaticTimestamp = DateTime.Now;
+        }
+
+        public DateTime Timestamp { get; private set; }
+
+        public ExpensiveTimestampedClass()
+        {
+            Timestamp = DateTime.Now;
+        }
+
+        public DateTime GetNow()
+        {
+            return DateTime.Now;
+        }
+    }
+}
